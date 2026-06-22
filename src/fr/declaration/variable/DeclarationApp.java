16623c0abcc0bdf8ec7bcc72ae90efcf0db36814
Alexandre Bertin <bertin.alexandre5@gmail.com>
2026-06-22 12:23:59 +0200

    TP3 terminé

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,22 @@
+package fr.declaration.variable;
+
+ public class DeclarationApp {
+    public static void main(String[] args) {
+
+    byte a = 1;
+    short b = 2;
+    int c = 5;
+    long d = 12358;
+    float  e = 1.3f;
+    double f = 15.23;
+    char g = 'a';
+    boolean h = true;
+    String i = "test";
+
+            System.out.println(i);
+
+        String randomString = "Voici le résultat d’un calcul : \n 1+5=6";
+        System.out.println(randomString);
+
+
+    }}
